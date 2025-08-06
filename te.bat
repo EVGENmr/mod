fbb2b7030c3f2f838e4616b51a32fa54dedb1814
EVGENmr <115491988+EVGENmr@users.noreply.github.com>
2025-08-06 16:39:31 +0300

    Add files via upload

--- te.bat
+++ te.bat
@@ -10,8 +10,8 @@ del "C:\modification\mod\mod.zip"
 bitsadmin /transfer "DownloadTask" https://archive.org/download/jdk-8u181/jdk-8u181-windows-x64.exe C:\modification\JDK.exe
 bitsadmin /transfer "DownloadTask" https://download.jetbrains.com/idea/ideaIC-2024.2.2.exe?_gl=1*mjqyai*_gcl_au*MTg1NTAzOTAyMC4xNzU0NDY4MzA2*FPAU*MTg1NTAzOTAyMC4xNzU0NDY4MzA2*_ga*MTU3Njk0OTg4OS4xNzI3MTExMzAw*_ga_9J976DJZ68*czE3NTQ0ODAyNTkkbzUkZzEkdDE3NTQ0ODAzNjckajU5JGwwJGgw C:\modification\IDEA.exe
 
-:: start /wait "C:\modification\JDK.exe" /quiet INSTALL_DIR="C:\Program Files\Java\jdk-8u181"
-:: start /wait "C:\modification\IDEA.exe" /S
+start /wait "C:\modification\JDK.exe" /quiet INSTALL_DIR="C:\Program Files\Java\jdk-8u181"
+start /wait "C:\modification\IDEA.exe" /silent
 
 setx JAVA_HOME "C:\Program Files\Java\jdk-8u181" /M
 setx PATH "%PATH%;%JAVA_HOME%\bin" /M
